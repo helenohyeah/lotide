9453eb88e40c9b645ea9b2c654454c03e2bd2752
helenohyeah <ouyang.helen@gmail.com>
2020-09-21 16:29:30 +0000

    Refactored assertArraysEqual to separate tests

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // pass
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3", 4], ["1", "2", "3", 4]); 
