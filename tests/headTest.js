ef07bff86d577c332e7903db764ddd4aadb1b192
helenohyeah <ouyang.helen@gmail.com>
2020-09-21 19:50:34 +0000

    Removed old test code

--- tests/headTest.js
+++ tests/headTest.js
@@ -1,13 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
-// assertEqual(head([5]), 5);
-// assertEqual(head(["Boo"]), "Boo");
-// assertEqual(head(), undefined);
-
 const assert = require('chai').assert;
 const head = require('../head');
 
