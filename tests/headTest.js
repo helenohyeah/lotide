52c1cdeae38e6a909c13eac640c51ecd1bb2ea40
helenohyeah <ouyang.helen@gmail.com>
2020-09-21 20:18:00 +0000

    Fixed linting issues

--- tests/headTest.js
+++ tests/headTest.js
@@ -2,23 +2,23 @@ const assert = require('chai').assert;
 const head = require('../head');
 
 describe("#head", () => {
-  it(`returns 1 for [ 1, 2, 3 ]`, () => {
+  it('returns 1 for [ 1, 2, 3 ]', () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
   
-  it (`returns 5 for [5]`, () => {
+  it('returns 5 for [5]', () => {
     assert.strictEqual(head([5]), 5);
   });
 
-  it (`returns "Hello" for ["Hello", "Lighthouse", "Labs"]`, () => {
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
 
-  it (`returns "Hello" for ["Boo"]`, () => {
+  it('returns "Hello" for ["Boo"]', () => {
     assert.strictEqual(head(["Boo"]), "Boo");
   });
 
-  it (`returns undefined for empty parameter`, () => {
+  it('returns undefined for empty parameter', () => {
     assert.strictEqual(head(), undefined);
   });
 });
--- tests/middleTest.js
+++ tests/middleTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const middle = require ('../middle');
+const middle = require('../middle');
 
 assertArraysEqual(middle([1]), []); // pass
 assertArraysEqual(middle([1, 2]), []); // pass
