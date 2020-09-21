c6b4e68cfcce0f3bf5b665eff687365d61a4435a
helenohyeah <ouyang.helen@gmail.com>
2020-09-21 16:34:46 +0000

    Added package.json

--- test/headTest.js
+++ test/headTest.js
@@ -3,7 +3,7 @@ const head = require('../head');
 
 
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
 assertEqual(head([5]), 5);
 assertEqual(head(["Boo"]), "Boo");
 assertEqual(head(), undefined);
