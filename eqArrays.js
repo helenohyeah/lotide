4ff7e3b9f75a8d52d3e5658d59da4bd33301be1d
helenohyeah <ouyang.helen@gmail.com>
2020-09-21 16:25:06 +0000

    Refactored eqArrays to separate tests

--- eqArrays.js
+++ eqArrays.js
@@ -17,11 +17,3 @@ const eqArrays = function(array1, array2) {
 };
 
 module.exports = eqArrays;
-
-// TEST CODE
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", 4]), false);
-// assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2"]), false);
